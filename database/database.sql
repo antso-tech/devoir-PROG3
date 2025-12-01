a3253c46be8f756a7b1e5c8b144e7f6d66eb39f8
antso.tech <hei.antso.2007@gmail.com>
2025-12-01 14:34:11 +0300

    remplacement de student en users

--- database/database.sql
+++ database/database.sql
@@ -1,15 +1,17 @@
 -- Active: 1760942016873@@localhost@5432@users
-CREATE TABLE STUDENT (
-    id_student SERIAL PRIMARY KEY,
+CREATE TABLE USERS (
+    id_users SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     firstname VARCHAR(50) NOT NULL,
     birthdate DATE NOT NULL,
-    student_group VARCHAR(50) 
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(8) NOT NULL
 );
 
-INSERT INTO STUDENT (name, firstname, birthdate, student_group) VALUES ('March', 'Jeanne', '1999-4-8', 'K2');
+INSERT INTO USERS (name, firstname, birthdate, email, password) VALUES ('March', 'Jeanne', '1999-4-8', 'Jeanne@gmail.com','j3anM@');
 
-SELECT * FROM STUDENT;
+
+SELECT * FROM USERS;
 
 
 
--- src/main/java/org/example/Users.java
+++ src/main/java/org/example/Users.java
@@ -1,8 +1,25 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Users {
+    String url = "jdbc:postgresql://localhost/users";
+    String users = "postgres";
+    String password = "ntsoa";
 
 
+
+    public void getAllUsers(){
+        try(Connection connection = DriverManager.getConnection(url,users,password)){
+            Statement st = connection.createStatement()
+        }catch (SQLException e){
+
+
+        }
+    }
     public static void main(String[] args) {
 
     }
