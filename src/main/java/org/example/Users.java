a3508546ad1b7df39890b831808990183a911764
antso.tech <hei.antso.2007@gmail.com>
2025-12-01 14:43:45 +0300

    initiation avec getAllUsers

--- src/main/java/org/example/Users.java
+++ src/main/java/org/example/Users.java
@@ -1,21 +1,27 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Users {
     String url = "jdbc:postgresql://localhost/users";
     String users = "postgres";
     String password = "ntsoa";
 
-
-
+    String selectQuery = "SELECT * FROM USERS";
     public void getAllUsers(){
         try(Connection connection = DriverManager.getConnection(url,users,password)){
-            Statement st = connection.createStatement()
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(selectQuery);
+
+            if(rs.next()){
+                System.out.println("{name : " + rs.getString("name")
+                        + ", firstname : " + rs.getString("firstname")
+                + ", email : " + rs.getString("email") + ", password : "
+                + rs.getString("password"));
+            }
+
         }catch (SQLException e){
+            throw new RuntimeException(e);
 
 
         }
