d101a4ceca3167adf0f42e774d61174488cd37e0
antso.tech <hei.antso.2007@gmail.com>
2025-12-01 14:17:16 +0300

    exemple d'insert pour commencer

--- database/database.sql
+++ database/database.sql
@@ -1,9 +1,15 @@
 -- Active: 1760942016873@@localhost@5432@users
 CREATE TABLE STUDENT (
-    id SERIAL PRIMARY KEY,
+    id_student SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     firstname VARCHAR(50) NOT NULL,
-    Birthdate VARCHAR(50) NOT NULL,
-    Group VARCHAR(50) NOT NULL
+    birthdate DATE NOT NULL,
+    student_group VARCHAR(50) 
 );
+
+INSERT INTO STUDENT (name, firstname, birthdate, student_group) VALUES ('March', 'Jeanne', '1999-4-8', 'K2');
+
+SELECT * FROM STUDENT;
+
+
 
